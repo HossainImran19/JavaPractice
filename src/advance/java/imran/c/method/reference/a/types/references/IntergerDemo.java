1eceb316b50ced237e31f2f48f5c80c10cb5a784
ImranHossain00 <imran188566@gmail.com>
2023-11-03 12:28:24 +0600

    method ref

---
+++ src/advance/java/imran/c/method/reference/a/types/references/IntergerDemo.java
@@ -0,0 +1,10 @@
+package advance.java.imran.c.method.reference.a.types.references;
+
+@FunctionalInterface
+public interface IntergerDemo {
+
+    String demo(String msg);
+    public static int apply(int a, int b) {
+        return a+b;
+    }
+}
