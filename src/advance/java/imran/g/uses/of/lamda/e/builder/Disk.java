108ccf64a6fb6671408d17177e48cf7a20a13f58
ImranHossain00 <imran188566@gmail.com>
2023-12-08 11:33:24 +0600

    Till Page 148

---
+++ src/advance/java/imran/g/uses/of/lamda/e/builder/Disk.java
@@ -0,0 +1,49 @@
+package advance.java.imran.g.uses.of.lamda.e.builder;
+
+public class Disk {
+    public enum Type {
+        SATA, PATA
+    }
+
+    private final int size;
+    private final int speed;
+    private final Type type;
+
+    public Disk(int size, int speed, Type type) {
+        this.size = size;
+        this.speed = speed;
+        this.type = type;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public static DiskBuilder disk() {
+        return size -> speed -> type -> new Disk(size, speed, type);
+    }
+
+    public interface DiskBuilder {
+        SpeedBuilder size(int size);
+    }
+
+    public interface SpeedBuilder {
+        TypeBuilder speed(int speed);
+    }
+
+    public interface TypeBuilder {
+        Disk type(Type type);
+
+        default Disk SATA() {
+            return type(Type.SATA);
+        }
+    }
+}
